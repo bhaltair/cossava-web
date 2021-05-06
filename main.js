582fcb49641e06fa756cf157c0c5fe5a4c87d6fd
bhaltair <njcit_wangqi@163.com>
2021-05-06 23:33:28 +0800

    update

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 $(document).ready(function () {
+  window.addEventListener("scroll", function () {
+    let t = $("body, html").scrollTop(); // 目前监听的是整个body的滚动条距离
+
+    if (t > 0) {
+      $(".outer2").addClass("outer2-active");
+    } else {
+      $(".outer2").removeClass("outer2-active");
+    }
+  });
+
   $(".outer4").click(function () {
     $(".nav").toggleClass("show");
   });
-
 
   $(".nav li").click(function () {
     $(this).siblings("li").removeClass("active"); // 删除其他兄弟元素的样式
