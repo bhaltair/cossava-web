cafd5422c624e2cdaa487290d8f9f867312da3a6
bhaltair <njcit_wangqi@163.com>
2021-05-09 23:40:07 +0800

    update

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
+  var width = window.innerWidth;
+
+  if (width > 1029) {
+    $("nav").css("display", "show");
+  }
+
   window.addEventListener("scroll", function () {
     let t = $("body, html").scrollTop(); // 目前监听的是整个body的滚动条距离
 
@@ -10,14 +16,33 @@ $(document).ready(function () {
   });
 
   $(".outer4").click(function () {
-    $(".nav").toggleClass("show");
+    $(".nav").slideToggle("slow");
   });
 
   $(".nav li").click(function () {
+    if (width < 1029) {
+      $(".nav").css("display", "none");
+    }
+
     $(this).siblings("li").removeClass("active"); // 删除其他兄弟元素的样式
 
     $(this).addClass("active"); // 添加当前元素的样式
+  });
 
-    $(".nav").toggleClass("show");
+  $(".scroll").on("click", function () {
+    //绑定点击事件
+    var tops = $(this).attr("href"); //获取对象
+    $("html,body")
+      .stop()
+      .animate({ scrollTop: $(tops).offset().top }, 800); //动画出炉
+  });
+
+  $(".header-logo").click(function () {
+    $("body, html").stop().animate(
+      {
+        scrollTop: 0,
+      },
+      500
+    );
   });
 });
